69af778e4fef76bba3d867d0248f839a0cd7c2f6
James Heggs <james.heggs@closconsultancy.com>
2018-11-23 20:31:45 +0000

    Initial JS tests and array of books

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 004-fundamentals-stock-example
+# Stock Manager Exercises
+
+In this set of exercises, we introduce a stock manager.
+
+The purpose of the stock manager is to maintain and provide information on the stock of books we have in the bookstore.
+
---
+++ test/stock.test.js
@@ -0,0 +1,32 @@
+const stockManager = require('../app/stockmanager');
+
+//
+// This test checks that the stock manager correctly reports that the
+// stock manager correctly reports that there are 5 different titles in stock.
+//
+test('Check that there are 5 titles in stock', () => {
+    expect(stockManager.getStock().length).toBe(5);
+});
+
+// 
+// This test checks that the stock manager can correctly fetch a book
+// by ISBN number
+//
+test('Check that the stock manager can fetch a book by ISBN', () => {
+
+    const isbnNUmberToCheck = "784-461";
+    const book = stockManager.getBookByIsbn(isbnNUmberToCheck);
+    expect(book.title).toBe("The Witches");
+});
+
+
+// 
+// This test checks that the stock manager can correctly report how many copies
+// are present for a particular book.
+// Each book is identified by an ISBN number
+//
+test('Check that the stock manager can report the number of copies available for a particular book', () => {
+
+    const isbnNUmberToCheck = "123-456";
+    expect(stockManager.getCopiesForBook(isbnNUmberToCheck)).toBe(3);
+});

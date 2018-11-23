105a5b5f6f41099f90617881ea13234803d4915c
James Heggs <james.heggs@closconsultancy.com>
2018-11-23 20:47:37 +0000

    Completed task document

--- test/stockmanager.test.js
+++ test/stockmanager.test.js
@@ -12,6 +12,9 @@ test('Check that there are 5 titles in stock', () => {
 // This test checks that the stock manager can correctly fetch a book
 // by ISBN number
 //
+// HINT For this one you should be able to use the ES6 filter method:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+//
 test('Check that the stock manager can fetch a book by ISBN', () => {
 
     const isbnNUmberToCheck = "784-461";
@@ -23,6 +26,7 @@ test('Check that the stock manager can fetch a book by ISBN', () => {
 // 
 // This test checks that the stock manager can correctly report how many copies
 // are present for a particular book.
+//
 // Each book is identified by an ISBN number
 //
 test('Check that the stock manager can report the number of copies available for a particular book', () => {
